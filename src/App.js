2b92b574551afcc2b305ba8462a3b89735fdda4e
Devintel <devintelartbay@gmail.com>
2022-03-14 05:16:03 -0700

    index changes and refractor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      
+      <p> Hello </p>
     </div>
   );
 }
 
-export default App;
