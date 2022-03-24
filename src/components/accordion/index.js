518b801e8d78fcbf977dd5bd6c4e404b68fe7b9a
Devintel <devintelartbay@gmail.com>
2022-03-24 02:02:11 -0700

    testing accordion

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -40,8 +40,15 @@ Accordion.Header = function AccordionHeader ({ children, ...restProps }) {
     const { toggleShow, setToggleShow } = useContext(ToggleContext);
 
     return (
-        <Header onclick= {() => setToggleShow(( toggleShow ) => !toggleShow )} { ...restProps }>
+        <Header 
+          onClick= {() => setToggleShow(( toggleShow ) => !toggleShow )} 
+          { ...restProps }>
           { children }
+          { toggleShow ? (
+              <img src="/images/icons/close-slim.png" alt="close" />
+          ) : (
+              <img src="/images/icons/add.png" alt="open" />
+          )}
         </Header>
     );
 };
@@ -49,5 +56,5 @@ Accordion.Header = function AccordionHeader ({ children, ...restProps }) {
 Accordion.Body = function AccordionBody ({ children, ...restProps }) {
     const { toggleShow } = useContext(ToggleContext);
 
-    return toggleShow ? <Body { ...restProps }>{ children }</Body> : null
+    return toggleShow ? <Body { ...restProps }>{ children }</Body> : null;
 };
